991dde540f30f05f404cb4502d242e81c754b204
Frank Dai <sd924@cornell.edu>
2024-03-23 16:26:17 -0400

    Implement basic fuctions for deck
    - Wrote Make functor

---
+++ lib/deck.mli
@@ -0,0 +1,23 @@
+module type Card = sig
+  type t
+
+  val compare : t -> t -> int
+  val to_string : t -> string
+end
+
+module type Deck = sig
+  type card
+  type t
+
+  val shuffle : t -> t
+  (** [shuffle d] is the deck [d] with its cards shuffled. *)
+
+  val draw : t -> card * t
+  (** [draw d] is the top card of the deck [d] and the deck with the top card
+      removed. *)
+
+  val to_string : t -> string
+  (** [to_string d] is a string representation of the deck [d]. *)
+end
+
+module Make (C : Card) : Deck with type card = C.t

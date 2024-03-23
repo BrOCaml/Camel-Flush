24002bcfa5225de1174e4ccb029c4cbf88373b6d
Mohamed <mtk67@cornell.edu>
2024-03-23 18:59:42 -0400

    Added function to generate full deck.

--- lib/card.mli
+++ lib/card.mli
@@ -20,14 +20,14 @@ type rank =
   | Ace
 
 type t = suit * rank
-(**representation type of the card*)
+(** representation type of the card *)
 
 val compare : t -> t -> int
-(**[compare a b] is the indication for order of a and b. If negative then a
-   should be before b, if positive then b should before a. 0 if equal*)
+(** [compare a b] is the indication for order of a and b. If negative then a
+    should be before b, if positive then b should before a. 0 if equal *)
 
 val to_string : t -> string
-(**[to_string t] is the string representation of t*)
+(** [to_string t] is the string representation of t *)
 
 val rank_to_int : rank -> int
-(**[rank_to_int r] is the integer representation of r*)
+(** [rank_to_int r] is the integer representation of r *)

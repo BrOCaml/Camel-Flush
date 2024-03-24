76a17903c689ca7e0444a04a62d6c622feab7ce5
Huajie Zhong <hz642@cornell.edu>
2024-03-24 00:11:51 -0400

    refactor combo and deck module

--- lib/deck.ml
+++ lib/deck.ml
@@ -13,7 +13,6 @@ module type Deck = sig
   val shuffle : t -> t
   val draw : t -> card * t
   val to_string : t -> string
-  val generate_deck : unit -> (Card.suit * Card.rank) list
 end
 
 module Make (C : Card) : Deck with type card = C.t = struct
@@ -42,28 +41,4 @@ module Make (C : Card) : Deck with type card = C.t = struct
     match deck with
     | [] -> "[]"
     | deck -> "[" ^ String.concat "; " (List.map C.to_string deck) ^ "]"
-
-  let generate_suit_deck suit =
-    let ranks =
-      [
-        Card.Two;
-        Three;
-        Four;
-        Five;
-        Six;
-        Seven;
-        Eight;
-        Nine;
-        Ten;
-        Jack;
-        Queen;
-        King;
-        Ace;
-      ]
-    in
-    List.map (fun rank -> (suit, rank)) ranks
-
-  let generate_deck () =
-    let suits = [ Card.Clubs; Diamonds; Hearts; Spades ] in
-    List.concat (List.map generate_suit_deck suits)
 end

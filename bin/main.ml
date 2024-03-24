bf3cf62f40eae108521e66a5d3eec8f24fb2dd2b
Frank Dai <sd924@cornell.edu>
2024-03-23 23:56:20 -0400

    Fix pseudo-random problem
    - Enable self_init for Random module
    - Added community card field

--- bin/main.ml
+++ bin/main.ml
@@ -3,7 +3,9 @@ module PokerDeck = Deck.Make (PokerCard)
 module PokerPlayer = Player.Make (PokerCard)
 module PokerGame = Game.Make (PokerCard) (PokerDeck) (PokerPlayer)
 
-let game = PokerGame.init 8
+let game =
+  PokerGame.init 8 |> PokerGame.deal |> PokerGame.deal
+  |> PokerGame.deal_community |> PokerGame.deal_community
+  |> PokerGame.deal_community
+
 let () = print_endline (PokerGame.to_string game)
-let deal_game = PokerGame.deal (PokerGame.deal game)
-let () = print_endline (PokerGame.to_string deal_game)

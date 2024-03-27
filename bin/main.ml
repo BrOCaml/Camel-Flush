fd1c77d1cb0cce0fc8360c47139286c6478eb855
Huajie Zhong <hz642@cornell.edu>
2024-03-27 10:18:20 -0400

    add user decision query

--- bin/main.ml
+++ bin/main.ml
@@ -8,4 +8,19 @@ let game =
   |> PokerGame.deal_community |> PokerGame.deal_community
   |> PokerGame.deal_community
 
+(* let user = PokerGame.get_nth_player game 0 *)
 let () = print_endline (PokerGame.to_string game)
+let _ = print_endline "It's your turn\nDo you want to fold, call, or raise?"
+
+let decision =
+  let rec ask () =
+    let line = String.uppercase_ascii (read_line ()) in
+    if line <> "FOLD" && line <> "CALL" && line <> "RAISE" then begin
+      print_endline "Invalid input. Please enter fold, call, or raise.";
+      ask ()
+    end
+    else line
+  in
+  String.capitalize_ascii (String.lowercase_ascii (ask ()))
+
+let () = print_endline ("You chose: " ^ decision)
